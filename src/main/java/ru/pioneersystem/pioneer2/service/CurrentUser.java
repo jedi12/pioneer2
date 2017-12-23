612a33b46699114d2934b4a41ed448142496d1c0
jedi12 <jedi12@mail.ru>
2017-12-23 15:25:01 +0700

    little change

--- src/main/java/ru/pioneersystem/pioneer2/service/CurrentUser.java
+++ src/main/java/ru/pioneersystem/pioneer2/service/CurrentUser.java
@@ -16,15 +16,12 @@ import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
 @Service("currentUser")
 @Scope(value="session", proxyMode = ScopedProxyMode.TARGET_CLASS)
-public class CurrentUser implements Serializable {
-    private static final long serialVersionUID = 1L;
-
+public class CurrentUser {
     private int screenHeight;
     private int screenWidth;
 
